e015879594341711fbc824e9842e588ec6fbf0d0
amrit123456789 <amritagarwal98@gmail.com>
2019-01-18 00:03:55 +0530

    fix passport login file
    Signed-off-by: amrit123456789 <amritagarwal98@gmail.com>

--- src/routes/pages/auth.js
+++ src/routes/pages/auth.js
@@ -1,12 +1,12 @@
 const route = require('express').Router()
-const passport = require('../../auth/passport')
+const passport = require('passport')
 const  Createsiteuser  = require('../../controllers/siteusers').createsiteuser
 
 route.get('/login', (req, res) => {
-  console.log("Abc")
+  //console.log("Abc")
 res.render('pages/auth/login')})
 route.get('/signup', (req, res) => {
-console.log("Abc")
+//console.log("Abc")
 res.render('pages/auth/signup')})
 
 route.post('/signup', async (req, res) => {
@@ -22,12 +22,22 @@ route.post('/signup', async (req, res) => {
 }
 )
 
-route.post('/login', (req,res) => {
-  console.log("sfsf");
-  passport.authenticate('local', {
-  failureRedirect: '/',
-  successRedirect: '/mainpage'
+route.get('/mainpage',(req,res)=>{
+  res.render('pages/mainpage')
+})
+
+//route.post('/login', (req,res) => {
+  // console.log("sfsf");
+  // passport.authenticate('local', {
+  // failureRedirect: '/',
+  // successRedirect: '/mainpage'
   //res.render('pages/mainpage')
-})})
+//})})
+
+route.post('/login', passport.authenticate('local', {
+  failureRedirect: '/auth/mainpage',
+  successRedirect: '/pages/mainpage',
+  //failureFlash: true
+}))
 
 module.exports = route
